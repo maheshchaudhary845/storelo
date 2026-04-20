8786aa399013411650de22cf6e8c4459d5949487
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-04-20 14:09:17 +0530

    add slug field

--- server/models/Product.js
+++ server/models/Product.js
@@ -10,6 +10,10 @@ const productSchema = new mongoose.Schema(
             type: String,
             required: true
         },
+        slug: {
+            type: String,
+            unique: true
+        },
         price: {
             type: Number,
             required: true
