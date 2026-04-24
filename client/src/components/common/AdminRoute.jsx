9c749c1c2c585e4094dc236359e376b535070ff7
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-04-24 23:46:22 +0530

    create admin protected route component

---
+++ client/src/components/common/AdminRoute.jsx
@@ -0,0 +1,12 @@
+import { useAuth } from "../../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+export function AdminRoute({children}){
+    const {user} = useAuth();
+    
+    if(!user) return <Navigate to="/login" />
+    
+    if(user && user.role !== "admin") return <Navigate to="/" />
+    
+    return children;
+}
