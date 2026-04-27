7578d2939f8ab94024fca95a8e4e187455649ecb
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-04-27 22:50:48 +0530

    add cart logo and enhance ui

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -5,7 +5,7 @@ import { useCart } from "../../context/CartContext";
 
 function Navbar() {
     const { user, logout } = useAuth();
-    const {totalQuantity} = useCart();
+    const { totalQuantity } = useCart();
 
     const navigate = useNavigate();
 
@@ -28,7 +28,14 @@ function Navbar() {
                         <>
                             <button onClick={() => navigate("/login")} className="py-1.75 px-4 border rounded-lg border-border cursor-pointer">Login</button>
                             <button onClick={() => navigate("/register")} className="py-1.75 px-4 border rounded-lg border-border cursor-pointer">Register</button>
-                            <button onClick={() => navigate("/cart")} className="py-1.75 px-4 border rounded-lg border-border cursor-pointer bg-accent">Cart{totalQuantity ? `(${totalQuantity})` : ''}</button>
+                            <button onClick={() => navigate("/cart")} className="py-1.75 px-4 border rounded-lg border-border cursor-pointer bg-accent relative"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" color="#ffffff" fill="none" stroke="#F0F0FF" stroke-width="1.5" stroke-linecap="round">
+                                <path d="M8 16L16.7201 15.2733C19.4486 15.046 20.0611 14.45 20.3635 11.7289L21 6" />
+                                <path d="M6 6H22" />
+                                <circle cx="6" cy="20" r="2" />
+                                <circle cx="17" cy="20" r="2" />
+                                <path d="M8 20L15 20" />
+                                <path d="M2 2H2.966C3.91068 2 4.73414 2.62459 4.96326 3.51493L7.93852 15.0765C8.08887 15.6608 7.9602 16.2797 7.58824 16.7616L6.63213 18" />
+                            </svg>{totalQuantity ? <span className="absolute top-0 right-0 bg-orange-500 rounded-full min-h-4 min-w-4 text-box-trim-both">{totalQuantity ? totalQuantity : ''}</span> : ''}</button>
                         </>
                         :
                         user.role === "admin" ?
@@ -38,7 +45,14 @@ function Navbar() {
                             </>
                             :
                             <>
-                                <button onClick={() => navigate("/cart")} className="py-1.75 px-4 border rounded-lg border-border cursor-pointer bg-accent">Cart{totalQuantity ? `(${totalQuantity})` : ''}</button>
+                                <button onClick={() => navigate("/cart")} className="py-1.75 px-4 border rounded-lg border-border cursor-pointer bg-accent relative"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" color="#ffffff" fill="none" stroke="#F0F0FF" stroke-width="1.5" stroke-linecap="round">
+                                    <path d="M8 16L16.7201 15.2733C19.4486 15.046 20.0611 14.45 20.3635 11.7289L21 6" />
+                                    <path d="M6 6H22" />
+                                    <circle cx="6" cy="20" r="2" />
+                                    <circle cx="17" cy="20" r="2" />
+                                    <path d="M8 20L15 20" />
+                                    <path d="M2 2H2.966C3.91068 2 4.73414 2.62459 4.96326 3.51493L7.93852 15.0765C8.08887 15.6608 7.9602 16.2797 7.58824 16.7616L6.63213 18" />
+                                </svg>{totalQuantity ? <span className="absolute top-0 right-0 bg-orange-500 rounded-full min-h-4 min-w-4 text-box-trim-both">{totalQuantity ? totalQuantity : ''}</span> : ''}</button>
                                 <button onClick={() => navigate("/orders")} className="py-1.75 px-4 border rounded-lg border-border cursor-pointer">My Orders</button>
                                 <button onClick={handleLogout} className="py-1.75 px-4 border rounded-lg border-border cursor-pointer text-danger">Logout</button>
                             </>
