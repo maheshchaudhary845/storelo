d4df97830d41860dde33f71697f56f4d5e86ec31
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-04-27 16:34:08 +0530

    add cart provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom"
 import { AuthProvider } from './context/AuthContext.jsx'
+import { CartProvider } from './context/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <CartProvider>
+          <App />
+        </CartProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
