dc3b45f33aebc9d3d15e9fed7291f99c389bb420
maheshchaudhary <maheshchaudhary845@gmail.com>
2026-07-13 17:12:51 +0530

    fix margin by adding block display to logo

--- client/src/components/admin/AdminSidebar.jsx
+++ client/src/components/admin/AdminSidebar.jsx
@@ -9,11 +9,10 @@ function AdminSidebar() {
         { id: 3, name: "Orders", path: "/admin/orders" }
     ]
 
-    console.log(location);
     return (
         <>
             <div className="side-panel flex-1 p-4 bg-card rounded-2xl border border-border h-fit">
-                <Link to={'/admin'} className="logo font-extrabold cursor-pointer mb-5">store<span className="text-accent">lo</span> <span className="text-[10px] font-mono text-accent">ADMIN</span></Link>
+                <Link to={'/admin'} className="logo block font-extrabold cursor-pointer mb-5">store<span className="text-accent">lo</span> <span className="text-[10px] font-mono text-accent">ADMIN</span></Link>
                 <div className="flex flex-col gap-1 text-sm">
                     {links.map(link => (
                         <Link key={link.id} to={link.path} className={`p-3 ${location.pathname === link.path ? "text-accent bg-accent-soft" : "text-muted"} rounded-xl cursor-pointer`}>{link.name}</Link>
